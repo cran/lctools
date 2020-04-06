5bcf2ed31342c5a019c5c090052493425d33dcbd
Stamatis Kalogirou <stamatis@lctools.science>
2020-04-06 08:42:06 +0000

    version 0.2-8

--- inst/doc/SpatialInequalities.R
+++ inst/doc/SpatialInequalities.R
@@ -1,9 +1,9 @@
-## ----message=FALSE-------------------------------------------------------
+## ----message=FALSE------------------------------------------------------------
 library(lctools)
 data(GR.Municipalities)
 names(GR.Municipalities@data)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 myDF<-cbind(GR.Municipalities@data$Income01,GR.Municipalities@data$X, GR.Municipalities@data$Y)
 myDF[!complete.cases(myDF),]
 myDF.new<-na.omit(myDF)
@@ -12,7 +12,7 @@ wt<-'Binary'
 sp.G<-spGini(myDF.new[,2:3],bw,myDF.new[,1],wt)
 knitr::kable(sp.G, format = "pandoc", digits = 5)
 
-## ----warning = FALSE-----------------------------------------------------
+## ----warning = FALSE----------------------------------------------------------
 spGini.Sim20<-mc.spGini(Nsim=19,bw,myDF.new[,1],myDF.new[,2],myDF.new[,3],wt)
 spGini.Sim20$pseudo.p
 
